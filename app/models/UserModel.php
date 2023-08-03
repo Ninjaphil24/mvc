259835670c11eccd8b2b3e6ac6b41739cbfebd1d
unknown <philipmodino@gmail.com>
2023-08-03 14:43:02 +0300

    Create UserModel v1

---
+++ app/models/UserModel.php
@@ -0,0 +1,36 @@
+<?php
+require('../mysqli_connect.php');
+
+function createEmployee($con, $first_name, $last_name, $email){
+
+    $query = "INSERT INTO users(first_name, last_name, email)
+            VALUES
+            (?,?,?)";
+
+$stmt = $con->prepare($query);
+
+$stmt->bind_param(
+    "sss",
+    $first_name,
+    $last_name,
+    $email
+);
+// Execute the prepared statement
+if ($stmt->execute()) {
+    // Success! Data inserted into the table.
+    header("Location: success.php"); // Redirect to confirmation.php
+    exit; // Exit to prevent further execution
+} else {
+    // Check if the error is due to duplicate entry (unique constraint violation)
+    if ($con->errno === 1062) {
+        $error_message = "Duplicate entry. Please check your input.";
+    } else {
+        // Other errors
+        $error_message = "Error: " . $stmt->error;
+    }
+}
+
+// Close the statement and database connection
+$stmt->close();
+$con->close();
+}
